060237c88c2a08123df0700fb7d14ffc3bef6eb5
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-09 19:46:35 +0900

    feat: Add useRecoilValue

--- src/utils/recoil/useUserInputKeywordState.ts
+++ src/utils/recoil/useUserInputKeywordState.ts
@@ -2,3 +2,8 @@ export const userInputKeywordState = atom<string>({
   key: 'UserInputKeyword',
   default: '',
 });
+
+//ユーザーが入力したキーワード
+export const useUserInputKeywordState = (): string => {
+  return useRecoilValue(userInputKeywordState);
+};
