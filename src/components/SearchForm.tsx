27f4915cb8b6ffb16a51faf6e482fb380c307d11
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-09 16:23:09 +0900

    styles: Add style CSS

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,2 +1,19 @@
   const [searchAreaKey, setSearchAreaKey] = useState<string>('');
   const [searchGenreKey, setSearchGenreKey] = useState<string>('');
+const Form = styled.form``;
+
+const Label = styled.label`
+  display: block;
+  font-size: 0.75rem;
+  font-weight: bold;
+  color: #172621;
+`;
+
+const TextField = styled.input`
+  display: block;
+  width: 100%;
+  padding: 0.5rem 0.75rem;
+  border-radius: 6px;
+`;
+
+const SearchButton = styled.button``;
