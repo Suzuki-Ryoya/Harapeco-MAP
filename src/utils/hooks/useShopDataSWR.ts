d25af432bb770d0941e1bbf777198280dde93168
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-09 21:21:12 +0900

    styles: Add comment for fallbackData

--- src/utils/hooks/useShopDataSWR.ts
+++ src/utils/hooks/useShopDataSWR.ts
@@ -1,6 +1,7 @@
 import useSWR from 'swr';
 import { fetcher } from '../fetcher';
 
+//TODO: fallbackDataの型をanyからレスポンスの型に変更する
 export const useShopDataSWR = (userSetKeyword: string, fallbackData: any) => {
   return useSWR(`api/gourmet/${userSetKeyword}`, fetcher, {
     fallbackData,
