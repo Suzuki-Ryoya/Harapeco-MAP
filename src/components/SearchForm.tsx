31e69b099e97cb7c699cbf15894636072115fba7
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-09 16:23:36 +0900

    feat: Create SeachForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,36 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export const SearchForm: React.FC = () => {
   const [searchAreaKey, setSearchAreaKey] = useState<string>('');
   const [searchGenreKey, setSearchGenreKey] = useState<string>('');
+  return (
+    <>
+      <Form>
+        <div>
+          <Label id="serach_area">エリア</Label>
+          <TextField
+            id="search_area"
+            value={searchAreaKey}
+            placeholder="市、地区"
+            onChange={(v) => setSearchAreaKey(v.target.value)}
+          ></TextField>
+        </div>
+        <div>
+          <Label id="serach_area">ジャンル</Label>
+          <TextField
+            id="search_area"
+            value={searchGenreKey}
+            placeholder="例)居酒屋"
+            onChange={(v) => setSearchGenreKey(v.target.value)}
+          ></TextField>
+        </div>
+        <SearchButton>検索</SearchButton>
+      </Form>
+    </>
+  );
+};
+
 const Form = styled.form``;
 
 const Label = styled.label`
