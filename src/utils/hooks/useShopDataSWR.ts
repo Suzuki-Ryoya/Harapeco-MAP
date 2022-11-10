eaa0eac21484559c100ae5717856916ac921a972
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-10 19:01:03 +0900

    fix: Change path to groumet

--- src/utils/hooks/useShopDataSWR.ts
+++ src/utils/hooks/useShopDataSWR.ts
@@ -3,7 +3,7 @@ import { fetcher } from '../fetcher';
 
 //TODO: fallbackDataの型をanyからレスポンスの型に変更する
 export const useShopDataSWR = (userSetKeyword: string, fallbackData: any) => {
-  return useSWR(`api/gourmet/${userSetKeyword}`, fetcher, {
+  return useSWR(`api/groumet/${userSetKeyword}`, fetcher, {
     fallbackData,
     refreshInterval: 30000,
   });
