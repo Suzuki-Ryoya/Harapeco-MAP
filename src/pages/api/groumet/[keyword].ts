044301848fb5d4391e054b9ab5fe71f933aad548
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-10 14:11:44 +0900

    feat: Add query to api request

--- src/pages/api/groumet/[keyword].ts
+++ src/pages/api/groumet/[keyword].ts
@@ -2,5 +2,15 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
+  const {
+    query: { keyword },
+  } = req;
+  if (
+    typeof process.env.API_URL_ROOT === 'undefined' ||
+    keyword === undefined
+  ) {
+    return;
+  }
+
 };
 export default handler;
