bce8ec00e7601abd653b02cfa5e8d88b2b5f450b
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-10 14:28:24 +0900

    feat: Create root URL handler

---
+++ src/pages/api/groumet/index.ts
@@ -0,0 +1,14 @@
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  const {
+    query: { keyword },
+  } = req;
+  if (typeof process.env.API_URL_ROOT === 'undefined') return;
+
+  const data = await fetcher(process.env.API_URL_ROOT);
+
+  res.end(JSON.stringify(data));
+};
+export default handler;
