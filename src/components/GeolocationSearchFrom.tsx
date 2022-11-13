f7cabf3b329907971a4fe4d9bb58fce7c168c9cd
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-13 15:13:15 +0900

    feat: Create rabgeOptions for groumet search

--- src/components/GeolocationSearchFrom.tsx
+++ src/components/GeolocationSearchFrom.tsx
@@ -2,6 +2,28 @@ import { locationFetcher } from '@/utils/geolocation';
 import { url } from 'inspector';
 import React from 'react';
 import useSWR from 'swr';
+const rangeOptions: SelectOption[] = [
+  {
+    label: '300m',
+    value: '1',
+  },
+  {
+    label: '500m',
+    value: '2',
+  },
+  {
+    label: '1000m',
+    value: '3',
+  },
+  {
+    label: '2000m',
+    value: '4',
+  },
+  {
+    label: '3000m',
+    value: '5',
+  },
+];
 export const GeolocationSearchForm: React.FC = () => {
   return (
     <>
