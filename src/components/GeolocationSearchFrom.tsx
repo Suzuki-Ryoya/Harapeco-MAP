933aa847abc6643a8140c3c401c85e5802d1c2eb
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-13 13:51:21 +0900

    feat: Create Geolocation Search Form

---
+++ src/components/GeolocationSearchFrom.tsx
@@ -0,0 +1,11 @@
+import { locationFetcher } from '@/utils/geolocation';
+import { url } from 'inspector';
+import React from 'react';
+import useSWR from 'swr';
+export const GeolocationSearchForm: React.FC = () => {
+  return (
+    <>
+      <div>sample</div>
+    </>
+  );
+};
