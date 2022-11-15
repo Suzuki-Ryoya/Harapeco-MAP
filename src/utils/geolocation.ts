3b7283da827621d96095ab22d0a2124c8144c69f
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-15 19:42:13 +0900

    refactor: Delete geoFetcher

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -7,27 +7,7 @@ import { useEffect, useState, useRef } from 'react';
 export const locationFetcher = () => {
   return new Promise(
     (res: (value?: Position) => void, rej: (reson?: PositionError) => void) => {
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(res, rej);
-      }
+      navigator.geolocation.getCurrentPosition(res, rej);
     },
   );
-};
-
-// TODO: 現在地取得に加えて範囲を指定したい
-export const geoFetcher = () => {
-  return new Promise((res, rej) => {
-    const onSuccess = async (position: any) => {
-      const latitude = String(position?.coords?.latitude);
-      const longitude = String(position?.coords?.longitude);
-      const result = await fetch(
-        `${env.API_URL_ROOT}&lat=${encodeURI(latitude)}&lng=${encodeURI(
-          longitude,
-        )}`,
-      );
-      const data = await result.json();
-      resolve(data);
-    };
-    navigator.geolocation.getCurrentPosition(onSuccess, rej);
-  });
 };
