1fcf32394297bc04dcf1fd89b7d70e73199d6d4c
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-15 12:23:32 +0900

    feat: Create Poster to fetch method POST

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -5,5 +5,15 @@ const fetcher = async (url: string): Promise<any> => {
 };
 
 export default fetcher;
+
+export const poster = async (url: string, data: any): Promise<any> => {
+  const response = await fetch(url, {
+    // 送信先URL
+    method: 'post', // 通信メソッド
+    headers: {
+      'Content-Type': 'application/json', // JSON形式のデータのヘッダー
+    },
+    body: JSON.stringify(data), // JSON形式のデータ
+  });
   return (await response.json()) as any;
 };
