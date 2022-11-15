7d5f833fdd812d4271fe16a935c90d3908685036
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-15 21:12:20 +0900

    styles: Add comment

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -4,6 +4,7 @@ import { env } from 'process';
 import { useEffect, useState, useRef } from 'react';
 
 // geolocation APIをラップ化してfetcherとしてuseSWRで呼び出す
+// TODO: APIディレクトリに入れたい
 export const locationFetcher = () => {
   return new Promise(
     (res: (value?: Position) => void, rej: (reson?: PositionError) => void) => {
