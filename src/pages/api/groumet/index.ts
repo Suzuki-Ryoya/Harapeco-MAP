2528a8aff387c6e67a0554b24b4b6808777978a0
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-15 12:28:48 +0900

    fix: Delete not use query

--- src/pages/api/groumet/index.ts
+++ src/pages/api/groumet/index.ts
@@ -1,15 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { fetcher } from '@/utils/fetcher';
+import fetcher from '@/utils/fetcher';
 
 // もとのROOT URLの取得
-
+// Next.js のAPIで動的ルーティング
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
-  const {
-    query: { keyword },
-  } = req;
   if (typeof process.env.API_URL_ROOT === 'undefined') return;
 
   const data = await fetcher(process.env.API_URL_ROOT);
