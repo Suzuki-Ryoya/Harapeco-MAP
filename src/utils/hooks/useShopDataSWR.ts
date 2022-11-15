1b72a1521125f994a57ff66a31c56aa31e32ca9b
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-15 12:21:45 +0900

    feat: Create custom useSWR

--- src/utils/hooks/useShopDataSWR.ts
+++ src/utils/hooks/useShopDataSWR.ts
@@ -1,5 +1,5 @@
 import useSWR from 'swr';
-import { fetcher } from '../fetcher';
+import fetcher, { poster } from '../fetcher';
 
 //TODO: fallbackDataの型をanyからレスポンスの型に変更する
 export const useShopDataSWR = (userSetKeyword: string, fallbackData: any) => {
@@ -7,4 +7,13 @@ export const useShopDataSWR = (userSetKeyword: string, fallbackData: any) => {
     fallbackData,
     refreshInterval: 30000,
   });
+};
+
+export const useShopListSWR = async (data: any) => {
+  // return useSWR(`api/search`, (url) => poster(url, { url: data }));
+  return await poster(`api/search`, { url: data });
+};
+
+export const useShopListsSWR = () => {
+  return useSWR(`api/search`, fetcher);
 };
