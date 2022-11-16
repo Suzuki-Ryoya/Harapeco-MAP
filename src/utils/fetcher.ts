2121150e9532de745484319dcee997df2264b34f
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-16 11:59:03 +0900

    fix: Delete poster

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -7,15 +7,3 @@ const fetcher = async (url: string): Promise<ShopListResponseType> => {
 };
 
 export default fetcher;
-
-export const poster = async (url: string, data: any): Promise<any> => {
-  const response = await fetch(url, {
-    // 送信先URL
-    method: 'post', // 通信メソッド
-    headers: {
-      'Content-Type': 'application/json', // JSON形式のデータのヘッダー
-    },
-    body: JSON.stringify(data), // JSON形式のデータ
-  });
-  return (await response.json()) as any;
-};
