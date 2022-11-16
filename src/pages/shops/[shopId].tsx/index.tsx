9230233c4414ba36dbd78921e41064d13e5f56ee
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-16 19:04:46 +0900

    chore: initial React.FC

--- src/pages/shops/[shopId].tsx/index.tsx
+++ src/pages/shops/[shopId].tsx/index.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import fetcher from '@/utils/fetcher';
 import { Shop, ShopDetail } from '@/types/shop';
 import styled from 'styled-components';
 
+const Sample: React.FC = () => {
   const router = useRouter();
   const shopId = String(router.query.shopId);
   const [shops, setShops] = useState<Shop[]>([]);
