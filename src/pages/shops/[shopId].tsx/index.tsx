8ac2bcc67cd3a3c34dc09b8b071e2cb9758b78e1
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-16 18:59:20 +0900

    feat: useEffect to get api/shop

--- src/pages/shops/[shopId].tsx/index.tsx
+++ src/pages/shops/[shopId].tsx/index.tsx
@@ -3,3 +3,17 @@ import { useEffect } from 'react';
 import fetcher from '@/utils/fetcher';
 import { Shop, ShopDetail } from '@/types/shop';
 import styled from 'styled-components';
+
+  const router = useRouter();
+  const shopId = String(router.query.shopId);
+  const [shops, setShops] = useState<Shop[]>([]);
+  useEffect(() => {
+    const fetchShops = async () => {
+      const response = await fetcher(
+        `http://localhost:3000/api/shop/?shopId=${encodeURI(shopId)}`,
+      );
+      setShops(response.results.shop);
+    };
+    fetchShops();
+  }, [shopId]);
+
