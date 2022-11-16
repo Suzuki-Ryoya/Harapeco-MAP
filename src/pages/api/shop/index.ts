a1042e123f353dce17e069c381b5978f6cfcc925
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-16 18:56:53 +0900

    feat: Create handler to get shop detail API

---
+++ src/pages/api/shop/index.ts
@@ -0,0 +1,31 @@
+import fetcher from '@/utils/fetcher';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  const {
+    query: { shopId },
+  } = req;
+
+  if (typeof process.env.API_URL_ROOT === 'undefined' || shopId === undefined) {
+    return;
+  }
+
+  const API_URL_ROOT = process.env.NEXT_PUBLIC_API_URL_ROOT;
+
+  const shopIdString: string = Array.isArray(shopId)
+    ? shopId.join(' ')
+    : shopId;
+
+  const API_URL =
+    typeof shopId === 'undefined' || shopId == null
+      ? API_URL_ROOT
+      : `${API_URL_ROOT}&id=${encodeURI(shopIdString)}`;
+  console.log(API_URL);
+  const data = await fetcher(API_URL);
+  console.log(data);
+  res.end(JSON.stringify(data));
+};
+export default handler;
