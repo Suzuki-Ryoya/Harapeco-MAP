d4ddb52a090e1b5b8c3c5298095565e787d2ba7d
Ryoya Suzuki <ryoya1206sr@icloud.com>
2022-11-16 18:57:24 +0900

    chore: Import tool

---
+++ src/pages/shops/[shopId].tsx/index.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import fetcher from '@/utils/fetcher';
+import { Shop, ShopDetail } from '@/types/shop';
+import styled from 'styled-components';
